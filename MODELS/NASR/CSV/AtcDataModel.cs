8d3edae26b8381f6c8d7c7dd2c45b3bfabbbc8d6
Kyle Sanders <ksanders7070@gmail.com>
2025-06-19 23:30:49 -0400

    Model Comments Complete

--- MODELS/NASR/CSV/AtcDataModel.cs
+++ MODELS/NASR/CSV/AtcDataModel.cs
@@ -46,43 +46,43 @@
             public string FacilityType { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Associated State Post Office Code
             /// _Src: All Atc_*.csv files(STATE_CODE)
             /// _MaxLength: 2
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Standard two letter abbreviation for US States and Territories.</remarks>
             public string? StateCode { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Location Identifier.
             /// _Src: All Atc_*.csv files(FACILITY_ID)
             /// _MaxLength: 4
             /// _DataType: string
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Unique 3-4 character alphanumeric identifier assigned to the Landing Facility or TRACON.</remarks>
             public string FacilityId { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Airport Associated City Name
             /// _Src: All Atc_*.csv files(CITY)
             /// _MaxLength: 40
             /// _DataType: string
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Name of city the airport is in.</remarks>
             public string City { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Country Code
             /// _Src: All Atc_*.csv files(COUNTRY_CODE)
             /// _MaxLength: 2
             /// _DataType: string
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Country Post Office Code Airport Located. Ex: "US"</remarks>
             public string CountryCode { get; set; }
 
         }
@@ -92,43 +92,41 @@
         public class AtcAtis : CommonFields
         {
             /// <summary>
-            /// NoTitleYet
+            /// ATIS Serial Number
             /// _Src: ATC_ATIS.csv(ATIS_NO)
             /// _MaxLength: (3,0)
             /// _DataType: int
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public int AtisNo { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Optional Description of Purpose
             /// _Src: ATC_ATIS.csv(DESCRIPTION)
             /// _MaxLength: 100
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Fulfilled by ATIS. Ex: "D-ATIS (ARR)"</remarks>
             public string? Description { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// ATIS Hours of Operation in Local Time
             /// _Src: ATC_ATIS.csv(ATIS_HRS)
             /// _MaxLength: 200
             /// _DataType: string
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Ex: "24" "1400-0400Z++ WKDAYS; 1600-0000Z++ WKENDS, CLSD FED HOLS."</remarks>
             public string AtisHrs { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// ATIS Phone Number
             /// _Src: ATC_ATIS.csv(ATIS_PHONE_NO)
             /// _MaxLength: 18
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public string? AtisPhoneNo { get; set; }
 
         }
@@ -138,223 +136,215 @@
         public class AtcBase : CommonFields
         {
             /// <summary>
-            /// NoTitleYet
+            /// ICAO Identifier
             /// _Src: ATC_BASE.csv(ICAO_ID)
             /// _MaxLength: 7
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public string? IcaoId { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Official Facility Name
             /// _Src: ATC_BASE.csv(FACILITY_NAME)
             /// _MaxLength: 50
             /// _DataType: string
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public string FacilityName { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// FAA Region Code
             /// _Src: ATC_BASE.csv(REGION_CODE)
             /// _MaxLength: 3
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>FAA region codes and their corresponding region names: ALASKA (AAL), CENTRAL (ACE), EASTERN (AEA), GREAT LAKES (AGL), NEW ENGLAND (ANE), NORTHWEST MOUNTAIN (ANM), SOUTHERN (ASO), SOUTHWEST (ASW), WESTERN-PACIFIC (AWP).</remarks>
             public string? RegionCode { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Operator Code of the Agency that Operates the Tower
             /// _Src: ATC_BASE.csv(TWR_OPERATOR_CODE)
             /// _MaxLength: 6
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>A=U.S. Air Force _ C=U.S. Coast Guard _ CITY=CITY _ COUNTY=COUNTY _ D=Canadian Ministry of Transport _ F=Federal Aviation Admin _ FCT=FAA Contract Tower _ G=Federal Gov't - Not U.S.A _ N=U.S. Navy _ NFCT=Non Fed Control Tower _ O=Other _ P=Private _ R=U.S. Army _ W=U.S. Weather Service _ X=Royal Canadian Air Force _ Z=Unknown</remarks>
             public string? TwrOperatorCode { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Radio Call used by Pilot to Contact Tower
             /// _Src: ATC_BASE.csv(TWR_CALL)
             /// _MaxLength: 26
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public string? TwrCall { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Hours of Tower Operation in Local Time
             /// _Src: ATC_BASE.csv(TWR_HRS)
             /// _MaxLength: 200
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Ex: "0800-1600 MON-FRI"</remarks>
             public string? TwrHrs { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Radio Call of Primary Approach Control
             /// _Src: ATC_BASE.csv(PRIMARY_APCH_RADIO_CALL)
             /// _MaxLength: 26
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Primary Facility That Furnishes APPCTRL SVC</remarks>
             public string? PrimaryApchRadioCall { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Primary Approach Control Facility ID
             /// _Src: ATC_BASE.csv(APCH_P_PROVIDER)
             /// _MaxLength: 700
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Facility ID (or Provider Description when Provider Type equals ‘S’) of the Agency That Operates the Primary Approach Control Facility/Functions</remarks>
             public string? ApchPProvider { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Provider Agency Type Code for Agency that Operates the Primary Approach Control Facility/Functions
             /// _Src: ATC_BASE.csv(APCH_P_PROV_TYPE_CD)
             /// _MaxLength: 1
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>A=Airport (with ATCT) _ C=ARTCC _ S=Special _ T=TRACON</remarks>
             public string? ApchPProvTypeCd { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Secondary Approach Control Facility ID
             /// _Src: ATC_BASE.csv(SECONDARY_APCH_RADIO_CALL)
             /// _MaxLength: 26
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Secondary Facility That Furnishes APPCTRL SVC</remarks>
             public string? SecondaryApchRadioCall { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Primary Approach Control Facility ID
             /// _Src: ATC_BASE.csv(APCH_S_PROVIDER)
             /// _MaxLength: 700
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Facility ID (or Provider Description when Provider Type equals ‘S’) of the Agency That Operates the Secondary Approach Control Facility/Functions</remarks>
             public string? ApchSProvider { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Secondary Approach Control Facility ID
             /// _Src: ATC_BASE.csv(APCH_S_PROV_TYPE_CD)
             /// _MaxLength: 1
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>A=Airport (with ATCT) _ C=ARTCC _ S=Special _ T=TRACON</remarks>
             public string? ApchSProvTypeCd { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Radio Call of Facility That Furnishes Primary Departure Control
             /// _Src: ATC_BASE.csv(PRIMARY_DEP_RADIO_CALL)
             /// _MaxLength: 26
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public string? PrimaryDepRadioCall { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Primary Departure Control Facility ID/Description
             /// _Src: ATC_BASE.csv(DEP_P_PROVIDER)
             /// _MaxLength: 700
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Facility ID (or Provider Description when Provider Type equals ‘S’) of the Agency That Operates the Primary Departure Control Facility/Functions</remarks>
             public string? DepPProvider { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Primary Departure Control Facility ID
             /// _Src: ATC_BASE.csv(DEP_P_PROV_TYPE_CD)
             /// _MaxLength: 1
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>A=Airport (with ATCT) _ C=ARTCC _ S=Special _ T=TRACON</remarks>
             public string? DepPProvTypeCd { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Radio Call of Facility That Furnishes Secondary Departure Control
             /// _Src: ATC_BASE.csv(SECONDARY_DEP_RADIO_CALL)
             /// _MaxLength: 26
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public string? SecondaryDepRadioCall { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Primary Departure Control Facility ID/Description
             /// _Src: ATC_BASE.csv(DEP_S_PROVIDER)
             /// _MaxLength: 700
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Facility ID (or Provider Description when Provider Type equals ‘S’) of the Agency That Operates the Secondary Departure Control Facility/Functions</remarks>
             public string? DepSProvider { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Primary Departure Control Facility ID
             /// _Src: ATC_BASE.csv(DEP_S_PROV_TYPE_CD)
             /// _MaxLength: 1
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>A=Airport (with ATCT) _ C=ARTCC _ S=Special _ T=TRACON</remarks>
             public string? DepSProvTypeCd { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Approach Departure Call associated with a Control Facility
             /// _Src: ATC_BASE.csv(CTL_FAC_APCH_DEP_CALLS)
             /// _MaxLength: 54
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public string? CtlFacApchDepCalls { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Agency Type Code that Operates the Control Facility
             /// _Src: ATC_BASE.csv(APCH_DEP_OPER_CODE)
             /// _MaxLength: 1
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>A=U.S. Air Force _ F=Federal Aviation Admin _ N=U.S. Navy _ R=U.S. Army</remarks>
             public string? ApchDepOperCode { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Hours of Operation of the Primary Control Facility
             /// _Src: ATC_BASE.csv(CTL_PRVDING_HRS)
             /// _MaxLength: 200
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public string? CtlPrvdingHrs { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Hours of Operation of the Secondary Control Facility
             /// _Src: ATC_BASE.csv(SECONDARY_CTL_PRVDING_HRS)
             /// _MaxLength: 200
             /// _DataType: string
             /// _Nullable: Yes
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public string? SecondaryCtlPrvdingHrs { get; set; }
 
         }
@@ -364,53 +354,51 @@
         public class AtcRmk : CommonFields
         {
             /// <summary>
-            /// NoTitleYet
+            /// Legacy Remark Element
             /// _Src: ATC_RMK.csv(LEGACY_ELEMENT_NUMBER)
             /// _MaxLength: 30
             /// _DataType: string
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>More of an ID than a Number; May contain other characters such as "TA21".</remarks>
             public string LegacyElementNumber { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// NASR Table name associated with Remark
             /// _Src: ATC_RMK.csv(TAB_NAME)
             /// _MaxLength: 30
             /// _DataType: string
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public string TabName { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// NASR Column name associated with Remark
             /// _Src: ATC_RMK.csv(REF_COL_NAME)
             /// _MaxLength: 30
             /// _DataType: string
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>ARPT_CTL_REMARKs are identified as ATC_REMARK. All other Non-specific remarks are identified as GENERAL_REMARK</remarks>
             public string RefColName { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Sequence number assigned to Reference Column Remark
             /// _Src: ATC_RMK.csv(REMARK_NO)
             /// _MaxLength: (3,0)
             /// _DataType: int
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
             public int RemarkNo { get; set; }
 
             /// <summary>
-            /// NoTitleYet
+            /// Remark Text
             /// _Src: ATC_RMK.csv(REMARK)
             /// _MaxLength: 1500
             /// _DataType: string
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Free Form Text that further describes a specific Information Item.</remarks>
             public string Remark { get; set; }
 
         }
@@ -420,13 +408,13 @@
         public class AtcSvc : CommonFields
         {
             /// <summary>
-            /// NoTitleYet
+            /// Services Provided to Satellite Airport
             /// _Src: ATC_SVC.csv(CTL_SVC)
             /// _MaxLength: 200
             /// _DataType: string
             /// _Nullable: No
             /// </summary>
-            /// <remarks>NoRemarksYet</remarks>
+            /// <remarks>Examples: "ARTS-IIIE" "CLASS B" "CONFLICT ALERT"</remarks>
             public string CtlSvc { get; set; }
 
         }
